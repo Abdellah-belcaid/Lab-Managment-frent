243ba672b91b8e8124453a5780c644150701717a
belcaid <belcaid.abdellah2001@gmail.com>
2023-10-04 16:45:08 +0200

    add backend url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080' // Replace with the actual base URL of your backend API
+  apiUrl: 'https://lab-managment-system.onrender.com/' // Replace with the actual base URL of your backend API
 };
